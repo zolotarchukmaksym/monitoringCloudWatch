8e3eca04639d2789538598870df1e57b8680f4f5
MaksymZolotarchuk <146109073+MaksymZolotarchuk@users.noreply.github.com>
2026-09-20 01:50:08 +0300

    Fix port to 8000 to match Target Group requirements

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     # Якщо в лабораторній замість ${custom_identifier} вказано конкретне значення - впишіть його
-    return 'Hello from the environment ${custom_identifier}!'
+    return 'Hello from the environment ar11voam!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
