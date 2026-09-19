41923a48446173377aa96ca9cfb59d42ec8c02dc
MaksymZolotarchuk <146109073+MaksymZolotarchuk@users.noreply.github.com>
2026-09-20 01:39:44 +0300

    Fix port to 8000 to match Target Group requirements

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello():
     return 'Hello from the environment ${custom_identifier}!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=8000)
