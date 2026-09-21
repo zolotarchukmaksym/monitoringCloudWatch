3807884c1c6503c7db331460be3a0a064f97904c
Maksym Zolotarchuk <zolotarchuk00@gmail.com>
2026-09-22 00:33:58 +0300

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-cd /home/ec2-user/flask-app
-nohup python3 app.py > /home/ec2-user/flask-app/app.log 2>&1 &
+cd /opt/flask-app
+nohup python3 app.py > /var/log/flask-app.log 2>&1 < /dev/null &
+exit 0
