3d502c0b86043dec9984215d17417d50a1996b8b
Maksym Zolotarchuk <zolotarchuk00@gmail.com>
2026-09-22 00:58:42 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "<h1>Flask app v1</h1><p>Hello from the environment ar11voam!</p>", 200
+def hello():
+    return 'Hello from the environment ar11voam!'
 
 @app.route('/health')
 def health():
-    return "OK", 200
+    return 'OK', 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
