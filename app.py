434772d9af9004d0dfe7f99b02b22fa4c57ea1c0
Maksym Zolotarchuk <zolotarchuk00@gmail.com>
2026-09-22 14:10:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 from flask import Flask
+import broken_module
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>Flask app v2 - STABLE</h1><p>Hello from the environment ar11voam!</p>", 200
+    return "<h1>Flask app v3 - BROKEN</h1>", 500
 
 @app.route('/health')
 def health():
-    return "OK", 200
+    return "FAIL", 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
